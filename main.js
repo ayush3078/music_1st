3f15ad67da8c87d0e924796fa6505d5341ae57f6
ayush3078 <105914399+ayush3078@users.noreply.github.com>
2023-04-01 12:57:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup() {
+    canvas=createCanvas(450,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video,0,0,450,400);
+}
